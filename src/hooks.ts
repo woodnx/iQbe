1253f547a7470f4022caba1fd0eadc0b852f386b
woodnext <shun20020611@yahoo.co.jp>
2023-05-09 10:28:23 +0900

    Add useInput hook

--- src/hooks.ts
+++ src/hooks.ts
@@ -1,5 +1,29 @@
+import React, { useState } from "react"
+
 export function fetcher(url: string) { 
   const baseUrl = 'http://localhost:9000/v2'
 
   return fetch(`${baseUrl}${url}`).then(res => res.json())
+}
+
+export const useInput = (
+  initialValue: number | string,
+): [
+  {
+    value: number | string,
+    onChange: (e: React.ChangeEvent<HTMLInputElement>) => void
+  },
+  () => void,
+] => {
+  const [ value, setValue ] = useState<number | string>(initialValue)
+
+  const props = { 
+    value, 
+    onChange: (e: React.ChangeEvent<HTMLInputElement>) => setValue(e.currentTarget.value) 
+  }
+  const resetValue = () => setValue(initialValue)
+
+  return [
+    props, resetValue
+  ]
 }
