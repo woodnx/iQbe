540d47456239257f5dcd4f823cac8e12f7ceaea1
woodnext <shun20020611@yahoo.co.jp>
2023-05-15 09:16:34 +0900

    Add App.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter as Router } from 'react-router-dom'
 import { MantineProvider } from '@mantine/core'
-import { Notifications } from '@mantine/notifications';
-import DetermineLayout from './determineLayout.tsx'
+import App from './App.tsx';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <MantineProvider>
-      <Notifications position="top-right"/>
-      <Router>
-        <DetermineLayout />
-      </Router>
+      <App />
     </MantineProvider>
   </React.StrictMode>,
 )
