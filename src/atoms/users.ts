7094722d79dd9b0da34a7cb9f268b20cf735f2a2
woodnext <shun20020611@yahoo.co.jp>
2023-05-15 15:23:02 +0900

    change atom directory

--- src/atoms/users.ts
+++ src/atoms/users.ts
@@ -2,6 +2,7 @@ import { atom } from "jotai";
 import { User } from "firebase/auth"
 
 export type FirebaseUser = User | null
+export type UserIdToken = string | null
 export type UserData = {
   isLogined: boolean,
   nickname: string,
@@ -10,4 +11,4 @@ export type UserData = {
 
 export const fireBaseUserAtom = atom<FirebaseUser>(null)
 
-export const userDataAtom = atom<UserData>(null)
+export const idTokenAtom = atom<string>('')
