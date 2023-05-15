0fd96b94fbeb5d3fda173a059a4909b548a19ba1
woodnext <shun20020611@yahoo.co.jp>
2023-05-15 15:21:34 +0900

    Add useFetch hook

--- src/hooks.ts
+++ src/hooks.ts
@@ -1,10 +1,5 @@
-import { useState } from "react"
-
-export function fetcher(url: string) { 
-  const baseUrl = 'http://localhost:9000/v2'
-
-  return fetch(`${baseUrl}${url}`).then(res => res.json())
-}
+import { useEffect, useState } from "react"
+import useUserStore from "./store/user"
 
 export const useInput = (
   initialValue: number | string,
@@ -26,4 +21,35 @@ export const useInput = (
   return [
     props, resetValue
   ]
+}
+
+export const useFetch = <T>(
+  url: string, 
+  init?: RequestInit
+) => {
+  const [data, setData] = useState<T>()
+  const [error, setError] = useState()
+  const [loading, setLoading] = useState(true)
+  const token = useUserStore((state) => state.idToken)
+  const baseUrl = 'http://localhost:9000/v2'
+
+  useEffect(() => {
+    if (!url) return
+    fetch(`${baseUrl}${url}`, {
+      ...init,
+      headers: {
+        "Authorization": `${token}`
+      }
+    })
+    .then(res => res.json())
+    .then(setData)
+    .then(() => setLoading(false))
+    .catch(setError)
+  }, [url])
+
+  return {
+    loading,
+    data,
+    error
+  }
 }
