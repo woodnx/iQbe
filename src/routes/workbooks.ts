f8bbaba5351670495762974d5e504b83d702ecbf
woodnext <shun20020611@yahoo.co.jp>
2023-06-26 14:24:57 +0900

    Add function at workbooks

--- src/routes/workbooks.ts
+++ src/routes/workbooks.ts
@@ -12,4 +12,20 @@ router.get('/', async (req, res) => {
   } 
 })
 
+router.get('/color', async (req, res) => {
+  try {
+    const results = await knex('workbooks')
+    .select(
+      { id: 'workbooks.id' },
+      { label: 'workbooks.name' }, 
+      { color: 'levels.color' }
+    )
+    .innerJoin('levels', 'workbooks.level_id', 'levels.id')
+
+    res.send(results)
+  } catch(err) {
+    console.error(err)
+  } 
+})
+
 module.exports = router
