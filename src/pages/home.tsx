734d4f3e32909bb2ea7f3248949e6f110846c560
woodnext <shun20020611@yahoo.co.jp>
2023-06-27 22:07:34 +0900

    Rename QuizCard to Quiz

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,11 +1,11 @@
-import QuizCardList from '../components/QuizCardList'
-import FilteringModal from '../components/FilteringModal'
+import QuizList from '../components/QuizList'
+import QuizControllBar from '../components/QuizControllBar'
 
 export default function Home() {
   return (
     <>
-      <FilteringModal/>
-      <QuizCardList/>
+      <QuizControllBar/>
+      <QuizList/>
     </>
   )
 }
