fea97b4edee539195ce3148a211302e8b48b2056
woodnext <shun20020611@yahoo.co.jp>
2023-07-09 10:26:41 +0900

    Add favorite feature

--- src/components/QuizFavoriteButton.tsx
+++ src/components/QuizFavoriteButton.tsx
@@ -1,14 +1,33 @@
 import { ActionIcon } from "@mantine/core";
 import { IconStar, IconStarFilled } from "@tabler/icons-react";
 import { useState } from "react";
+import axios from "../plugins/axios";
 
-export default function QuizFavoriteButton() {
-  const [ isFavorite, setFavorite ] = useState(false)
+interface QuizFavoriteButtonProps {
+  isFavorite: boolean,
+  quizId: number
+}
+
+export default function QuizFavoriteButton({ 
+  isFavorite: innerIsFavorite, 
+  quizId 
+}: QuizFavoriteButtonProps) {
+  const [ isFavorite, setFavorite ] = useState(innerIsFavorite)
+
+  const addFavoriteList = async () => {
+    if (isFavorite) {
+      const status = await axios.delete('/favorites', { data: { quizId } }).then(res => res.status)
+      if (status === 204) setFavorite(!isFavorite)
+    }else {
+      const status = await axios.post('/favorites', { quizId }).then(res => res.status)
+      if (status === 201) setFavorite(!isFavorite)
+    }
+  }
 
   return (
     <ActionIcon 
       color={isFavorite ? 'yellow.5' : 'gray'}
-      onClick={() => setFavorite(!isFavorite)} 
+      onClick={addFavoriteList} 
     >
       {isFavorite ? <IconStarFilled/> : <IconStar/>}
     </ActionIcon>
