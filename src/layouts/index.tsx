f09176c1206c336114cb3bb231f8fdec482d1361
woodnext <shun20020611@yahoo.co.jp>
2023-07-14 11:58:39 +0900

    Add practice page

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -2,6 +2,7 @@ import { Route, Routes, useLocation } from "react-router-dom"
 import Home from "../pages/home"
 import Login from "../pages/login"
 import Search from "../pages/search"
+import Practice from "../pages/practice"
 import Favorite from "../pages/favorite"
 import DefaultLayout from "./default"
 import DraftLayout from "./draft"
@@ -17,6 +18,7 @@ export default function Root() {
       <Route path="/" element={<Layout/>}>
         <Route index element={<Home />}/>
         <Route path="/search" element={<Search />}/>
+        <Route path="/practice" element={<Practice />}/>
         <Route path="/favorite" element={<Favorite />}/>
         <Route path="/login" element={<Login />}/>
       </Route>
---
+++ src/pages/practice.tsx
@@ -0,0 +1,5 @@
+export default function Practice() {
+  return (
+    <div>Practice Page</div>
+  )
+}
