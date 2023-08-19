4eb49313517615b74b6811169befddc2761c8d24
woodnext <shun20020611@yahoo.co.jp>
2023-08-19 16:21:48 +0900

    Add UserStatus

---
+++ src/hooks/useUserStatus.ts
@@ -0,0 +1,42 @@
+import useSWR from 'swr';
+import axios from '../plugins/axios';
+import { UserStatus } from '../types';
+import dayjs, { Period } from '../plugins/dayjs';
+
+const fetcher = (url: string) => (
+  axios.get<UserStatus[]>(`${url}`).then(res => res.data)
+);
+
+const defineLabel = (start: string, period: Period): string => {
+  if (period === 'month'){
+    const month = dayjs(start).format('MMM');
+    return month;
+  } else {
+    const date = dayjs(start).date();
+    const month = dayjs(start).month();
+    return `${month+1}/${date}`;
+  }
+}
+
+const useUserStatus = (date: string, period?: Period) => {
+  const p = period || 'day';
+  const { data, isLoading, error } = useSWR(`analysis/status/${date}/${p}`, fetcher);
+
+  const userStatus = data?.map(status => {
+    const label = defineLabel(status.start, p);
+    const total = status.right + status.wrong + status.through;
+    return {
+      label,
+      total,
+      ...status
+    }
+  }).reverse();
+  
+  return {
+    userStatus,
+    isLoading,
+    error
+  }
+}
+
+export default useUserStatus;
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,7 +1,14 @@
+import { Grid } from "@mantine/core";
+import ActivityStatus from "../components/ActivityStatus";
+
 export default function Home() {
   return (
     <>
-      this is home.
+      <Grid>
+        <Grid.Col span={6}>
+          <ActivityStatus/>
+        </Grid.Col>
+      </Grid>
     </>
   )
 }
