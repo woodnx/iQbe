c109ef7cd1fbfa0397d9ad7b9886d82cae919019
woodnext <shun20020611@yahoo.co.jp>
2023-08-20 15:26:04 +0900

    Add coloring props

--- src/components/QuizList.tsx
+++ src/components/QuizList.tsx
@@ -2,22 +2,23 @@ import QuizCard from "./QuizCard"
 import { Quiz } from "../types"
 
 interface QuizListProps {
-  quizzes: Quiz[]
+  quizzes: Quiz[],
+  coloring?: boolean,
 }
 
-export default function QuizList(props: QuizListProps) {
+export default function QuizList({
+  quizzes,
+  coloring,
+}: QuizListProps) {
   return (
     <>
-      {props.quizzes?.map((quiz, idx) => (
+      {quizzes?.map((quiz, idx) => (
         <QuizCard 
           key={idx}
           index={idx+1}
           quiz={quiz}
-          styles={(theme) => ({
-            root: {
-              marginBottom: theme.spacing.xs
-            }
-          })}
+          coloring={coloring}
+          mb={10}
         />
       ))}
     </>
