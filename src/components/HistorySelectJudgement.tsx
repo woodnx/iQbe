40baf2ea26a4e45f2992d323b8f69e766cc240bb
woodnext <shun20020611@yahoo.co.jp>
2023-08-21 18:48:09 +0900

    Add history page

---
+++ src/components/HistorySelectJudgement.tsx
@@ -0,0 +1,52 @@
+import HistorySelectButton from "./HistorySelectButton";
+import { Judgement } from "../types";
+
+interface Props {
+  judgements: Judgement[],
+  right: number,
+  wrong: number,
+  throgh: number,
+  onSelect: (judgements: Judgement[]) => void,
+}
+
+export default function HistorySelectJudgement({
+  judgements,
+  right,
+  wrong,
+  throgh,
+  onSelect,
+}: Props) {
+  const setJudgements = (judgement: Judgement) => {
+    const include = judgements.includes(judgement);
+    if (include) onSelect(judgements.filter(j => j !== judgement));
+    else onSelect([ ...judgements, judgement ]);
+  }
+
+  return (
+    <>
+      <HistorySelectButton 
+        label="Right" 
+        color="red" 
+        count={right}
+        isSelect={judgements.includes(1)}
+        divide
+        onClick={() => setJudgements(1)}
+      />
+      <HistorySelectButton 
+        label="Wrong" 
+        color="blue" 
+        count={wrong} 
+        isSelect={judgements.includes(0)}
+        divide
+        onClick={() => setJudgements(0)}
+      />
+      <HistorySelectButton 
+        label="Through" 
+        color="gray" 
+        count={throgh}
+        isSelect={judgements.includes(2)}
+        onClick={() => setJudgements(2)}
+      />
+    </>  
+  )
+}
