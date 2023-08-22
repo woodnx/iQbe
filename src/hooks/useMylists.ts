b05408684e44c35b94a2d3cbd2386335cf9bec6d
woodnext <shun20020611@yahoo.co.jp>
2023-08-22 19:47:13 +0900

    Add Mylist adding processing

---
+++ src/hooks/useMylists.ts
@@ -0,0 +1,15 @@
+import useSWR from 'swr';
+import axios from '../plugins/axios';
+import { MylistInformation } from '../types';
+
+const fetcher = (url: string) => axios.get<MylistInformation[]>(url).then(res => res.data); 
+
+export const useMylistInfomations = () => {
+  const { data: mylists, isLoading, error } = useSWR('/mylists', fetcher);
+
+  return {
+    mylists,
+    isLoading,
+    error
+  }
+}
