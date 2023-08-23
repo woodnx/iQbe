441473b86f1ba21aac4249484277566f37cd8a39
woodnext <shun20020611@yahoo.co.jp>
2023-08-23 14:28:59 +0900

    Add mutate

--- src/hooks/useMylists.ts
+++ src/hooks/useMylists.ts
@@ -5,11 +5,12 @@ import { MylistInformation } from '../types';
 const fetcher = (url: string) => axios.get<MylistInformation[]>(url).then(res => res.data); 
 
 export const useMylistInfomations = () => {
-  const { data: mylists, isLoading, error } = useSWR('/mylists', fetcher);
+  const { data: mylists, isLoading, error, mutate } = useSWR('/mylists', fetcher);
 
   return {
     mylists,
     isLoading,
-    error
+    error,
+    mutate
   }
 }
