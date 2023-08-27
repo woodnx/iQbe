0e379c1baec0efbddd90b5d8ee32078b024e8ba0
woodnext <shun20020611@yahoo.co.jp>
2023-08-28 00:35:46 +0900

    Fix api's auth error

--- src/hooks/useMylists.ts
+++ src/hooks/useMylists.ts
@@ -4,8 +4,10 @@ import { MylistInformation } from '../types';
 
 const fetcher = (url: string) => axios.get<MylistInformation[]>(url).then(res => res.data); 
 
-export const useMylistInfomations = () => {
-  const { data: mylists, isLoading, error, mutate } = useSWR('/mylists', fetcher);
+export const useMylistInfomations = (shouldFetch = true) => {
+  const { data: mylists, isLoading, error, mutate } = useSWR(
+    shouldFetch ? '/mylists' : null, 
+  fetcher);
 
   return {
     mylists,
--- src/plugins/axios.ts
+++ src/plugins/axios.ts
@@ -1,6 +1,5 @@
 import _axios from "axios";
 import useUserStore from "../store/user";
-import { auth, checkFirebaseAuth } from "./firebase";
 
 const axios = _axios.create({
   baseURL: 'http://localhost:9000/v2',
@@ -15,13 +14,10 @@ const axios = _axios.create({
 axios.interceptors.request.use(async (request) => {
   //リクエスト前に毎回idTokenを取得する
   const idToken = useUserStore.getState().idToken;
-  const user = await checkFirebaseAuth(auth);
-
-  if (!user) return request;
   
   request.headers.Authorization = idToken;
   
-  return request
+  return request;
 },(error) => {
   // リクエスト エラーの処理
   return Promise.reject(error);
