9c41fcd7f70b19e20a5776d7e03a40d7986c1488
woodnext <shun20020611@yahoo.co.jp>
2023-08-28 00:35:46 +0900

    Fix api's auth error

--- src/store/user.ts
+++ src/store/user.ts
@@ -1,30 +1,30 @@
 import { StoreApi, UseBoundStore, create } from "zustand";
-import { User, getIdToken, onAuthStateChanged } from "firebase/auth"
-import { auth } from "../plugins/firebase";
+import { User, getIdToken } from "firebase/auth"
+import { auth, checkFirebaseAuth } from "../plugins/firebase";
 
-export type FirebaseUser = User | null
-export type UserIdToken = string | null
+export type FirebaseUser = User | null;
+export type UserIdToken = string | null;
 export type UserState = {
   nickname: string,
   photoUrl: string,
-  idToken: string,
-  setIdToken: (token?: string) => void
-}
+  idToken: string | null,
+  setIdToken: () => void
+};
 
 const useUserStore: UseBoundStore<StoreApi<UserState>> = create<UserState>((set) => ({
   nickname: '',
   photoUrl: '',
-  idToken: '',
-  setIdToken: (token?: string) => {
-    if (!!token) set({ idToken: token })
+  idToken: null,
+  setIdToken: async () => {
+    const user = await checkFirebaseAuth(auth);
+    if (!user) { 
+      set({ idToken: null });
+      return;
+    }
 
-    onAuthStateChanged(auth, async (user) => {
-      if (!user) return;
-
-      const idToken = await getIdToken(user, true)
-      set({ idToken })
-    }) 
+    const idToken = await getIdToken(user, true);
+    set({ idToken });
   },
-}))
+}));
 
-export default useUserStore
+export default useUserStore;
