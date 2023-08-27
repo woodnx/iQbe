576b67bee1a232aa843693b1806a721ebef36968
woodnext <shun20020611@yahoo.co.jp>
2023-08-27 23:25:46 +0900

    Add semicolon

--- src/plugins/axios.ts
+++ src/plugins/axios.ts
@@ -1,6 +1,6 @@
-import _axios from "axios"
-import useUserStore from "../store/user"
-import { auth, checkFirebaseAuth } from "./firebase"
+import _axios from "axios";
+import useUserStore from "../store/user";
+import { auth, checkFirebaseAuth } from "./firebase";
 
 const axios = _axios.create({
   baseURL: 'http://localhost:9000/v2',
@@ -10,21 +10,21 @@ const axios = _axios.create({
     // 'Authorization': `Bearer ${idToken}`,
   },
   responseType: 'json',
-})
+});
 
 axios.interceptors.request.use(async (request) => {
   //リクエスト前に毎回idTokenを取得する
-  const idToken = useUserStore.getState().idToken
-  const user = await checkFirebaseAuth(auth)
+  const idToken = useUserStore.getState().idToken;
+  const user = await checkFirebaseAuth(auth);
 
   if (!user) return request;
   
-  request.headers.Authorization = idToken
+  request.headers.Authorization = idToken;
   
   return request
 },(error) => {
   // リクエスト エラーの処理
   return Promise.reject(error);
-})
+});
 
-export default axios
+export default axios;
