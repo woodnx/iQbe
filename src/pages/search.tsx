687396abaf2ce17e10966d12d34207ea4ec9afc2
woodnext <shun20020611@yahoo.co.jp>
2023-08-29 02:22:27 +0900

    Tune performance: Filtering Modalの表示が重くなる症状を修正

--- src/pages/search.tsx
+++ src/pages/search.tsx
@@ -7,12 +7,10 @@ import { KeywordOption, QuizRequestParams } from '../types'
 import { useState } from 'react'
 import QuizPagination from '../components/QuizPagination'
 import QuizShuffleButton from '../components/QuizShuffleButton'
-import { useDisclosure } from '@mantine/hooks'
 
 export default function Search() {
   const [ params, setParams ] = useState<QuizRequestParams>({perPage: 100})
   const [activePage, setPage] = useState(1);
-  const [ opened, { open, close } ] = useDisclosure(false);
   const { quizzes } = useQuizzes(params)
 
   const size = !!quizzes ? quizzes[0].size : 0
@@ -35,7 +33,6 @@ export default function Search() {
       keyword, 
       keywordOption,
     })
-    close();
   }
 
   const toShuffle = (
@@ -65,9 +62,6 @@ export default function Search() {
           <Group>
             <FilteringModal
               apply={toFilter}
-              opened={opened}
-              onOpen={open}
-              onClose={close}
             />
             <QuizShuffleButton
               apply={toShuffle}
