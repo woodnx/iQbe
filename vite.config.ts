b46785655e67619482cf9720c01c1f03df2f18f8
woodnext <shun20020611@yahoo.co.jp>
2023-08-31 00:00:19 +0900

    Add pwa config

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -8,5 +9,46 @@ export default defineConfig({
 			"^/api/.*": "http://localhost:4000",
 		},
 	},
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      includeAssets: [
+        'offline.html',
+        'favicon.ico',
+        'iqbe-icon.png',
+      ],
+      manifest: {
+        theme_color: "#339AF0",
+        background_color: "#ffffff",
+        display: "browser",
+        scope: "/",
+        start_url: "/",
+        name: "iQbe",
+        short_name: "iqbe",
+        icons: [
+          {
+            src: "/icon-192x192.png",
+            sizes: "192x192",
+            type: "image/png"
+          },
+          {
+            src: "/icon-256x256.png",
+            sizes: "256x256",
+            type: "image/png"
+          },
+          {
+            src: "/icon-384x384.png",
+            sizes: "384x384",
+            type: "image/png"
+          },
+          {
+            src: "/icon-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: 'any maskable',
+          }
+        ]
+      }
+    }),
+  ],
 })
