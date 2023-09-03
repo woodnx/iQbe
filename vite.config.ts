92e359ccc6f85ff70cf0f32429027b7692cf1cd2
woodnext <shun20020611@yahoo.co.jp>
2023-09-04 08:55:52 +0900

    fix pwa setting for ios

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import { VitePWA } from 'vite-plugin-pwa'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
+import myManifest from './manifest.json';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -20,37 +21,7 @@ export default defineConfig({
       devOptions: {
         enabled: true,
       },
-      manifest: {
-        theme_color: "#339AF0",
-        background_color: "#ffffff",
-        scope: "/",
-        start_url: "/",
-        name: "iQbe",
-        short_name: "iqbe",
-        icons: [
-          {
-            src: "/icon-192x192.png",
-            sizes: "192x192",
-            type: "image/png"
-          },
-          {
-            src: "/icon-256x256.png",
-            sizes: "256x256",
-            type: "image/png"
-          },
-          {
-            src: "/icon-384x384.png",
-            sizes: "384x384",
-            type: "image/png",
-          },
-          {
-            src: "/icon-512x512.png",
-            sizes: "512x512",
-            type: "image/png",
-            purpose: 'any maskable',
-          }
-        ]
-      }
+      manifest: myManifest,
     }),
   ],
 })
