eb2bc32ee3d1bec23a9fd8245920e6f0e77c80a8
woodnext <shun20020611@yahoo.co.jp>
2023-11-09 19:32:02 +0900

    Add login modals

---
+++ packages/frontend/src/components/UserSignupModal.tsx
@@ -0,0 +1,63 @@
+import { useNavigate } from "react-router-dom";
+import { Button, Center, Paper, PasswordInput, TextInput } from "@mantine/core";
+import { useInput } from "../hooks";
+import { notifications } from "@mantine/notifications";
+import { signupUser } from "../plugins/auth";
+
+export function UserSignupModal() {
+  const [ passwordProps, resetPassword ] = useInput('');
+  const [ usernameProps, resetUsername ] = useInput('');
+  const navigate = useNavigate();
+
+  const submit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    try { 
+      await signupUser(usernameProps.value, passwordProps.value)
+      .then(async (_user) => {
+        navigate('/');
+      });
+  
+      resetPassword();
+      resetUsername();
+    } catch {
+      notifications.show({
+        title: 'Login Error',
+        message: 'No such user',
+        color: 'red',
+        withBorder: true,
+      });
+    }
+  };
+
+  return (
+    <Paper>
+      <Center>
+        <form onSubmit={submit}>
+          <TextInput 
+            {...usernameProps}
+            placeholder="Your username"
+            label="Username"
+            radius="xl"
+            size="md"
+          />
+          <PasswordInput
+            {...passwordProps}
+            placeholder="Your Password"
+            label="Password"
+            radius="xl"
+            size="md"
+            mt="sm"
+          />
+          <Button 
+            fullWidth 
+            type="submit" 
+            mt="lg"
+          >
+            Login
+          </Button>
+        </form>
+      </Center>
+    </Paper>
+  );
+}
--- packages/frontend/src/pages/login.tsx
+++ packages/frontend/src/pages/login.tsx
@@ -1,74 +1,28 @@
-import { useNavigate } from "react-router-dom";
-import { auth } from "../plugins/firebase";
-import { signInWithEmailAndPassword } from "firebase/auth";
-import { Button, Center, Grid, Paper, PasswordInput, TextInput } from "@mantine/core";
-import { useInput } from "../hooks";
+import { Button, Center, Modal, Stack } from "@mantine/core";
+import { useDisclosure } from "@mantine/hooks";
+import { UserLoginModal } from '../components/UserLoginModal';
 import Logo from "../components/Logo";
-import { notifications } from "@mantine/notifications";
-import { useIsMobile } from "../contexts/isMobile";
+import { UserSignupModal } from "../components/UserSignupModal";
 
 export default function Login() {
-  const [ passwordProps, resetPassword ] = useInput('');
-  const [ emailProps, resetEmail ] = useInput('');
-  const isMobile = useIsMobile();
-  const navigate = useNavigate();
-
-  const submit = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-
-    try { 
-      await signInWithEmailAndPassword(auth, String(emailProps.value), String(passwordProps.value))
-      .then(async (_userCredential) => {
-        navigate('/');
-      });
-  
-      resetPassword();
-      resetEmail();
-    } catch {
-      notifications.show({
-        title: 'Login Error',
-        message: 'Wrong e-mail or password',
-        color: 'red',
-        withBorder: true,
-      });
-    }
-  };
+  const [ loginOpened, { open: loginOpen, close: loginClose } ] = useDisclosure(false);
+  const [ signupOpened, { open: signupOpen, close: signupClose } ] = useDisclosure(false);
 
   return (
-    <Paper shadow="md" p="35px" radius="lg">
-      <Grid>
-        <Grid.Col span={isMobile ? 12 : 4}>
-          <Center>
-            <Logo width={160} />
-          </Center> 
-        </Grid.Col>
-        <Grid.Col span={isMobile ? 12 : 8}>
-          <form onSubmit={submit}>
-            <TextInput 
-              {...emailProps}
-              placeholder="Your Email"
-              label="Email"
-              radius="xl"
-              size="md"
-            />
-            <PasswordInput
-              {...passwordProps}
-              placeholder="Your Password"
-              label="Password"
-              radius="xl"
-              size="md"
-              mt="sm"
-            />
-            <Button 
-              fullWidth 
-              type="submit" 
-              mt="lg"
-            >
-              Login
-            </Button>
-          </form>
-        </Grid.Col>
-      </Grid>
-    </Paper>
+    <Center>
+      <Stack justify="center" w={500}>
+        <Center>
+          <Logo width={160} horizonal/>
+        </Center>
+        <Button onClick={loginOpen}>Login</Button>
+        <Button onClick={signupOpen}>New Registration</Button>
+      </Stack>
+      <Modal opened={loginOpened} onClose={loginClose} title="Login" w={500}>
+        <UserLoginModal />
+      </Modal>
+      <Modal opened={signupOpened} onClose={signupClose} title="New Registration">
+        <UserSignupModal/>
+      </Modal>
+    </Center>
   );
 }
