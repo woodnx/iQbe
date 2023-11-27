cc938b78a819a730c11f54606741b77676c76330
woodnext <shun20020611@yahoo.co.jp>
2023-11-27 18:04:08 +0900

    パスワードリセット機能を実装

---
+++ packages/frontend/src/components/ResetPasswordModal.tsx
@@ -0,0 +1,65 @@
+import axios from "@/plugins/axios";
+import { Alert, Button, TextInput } from "@mantine/core";
+import { useForm } from "@mantine/form";
+import { ContextModalProps } from '@mantine/modals';
+import { IconAlertCircle } from "@tabler/icons-react";
+
+interface SubmitValue {
+  username: string,
+  email: string
+}
+
+export default function ResetPasswordModal({ context, id, innerProps: _ }: ContextModalProps) {
+  const form = useForm({
+    initialValues: {
+      username: '',
+      email: '',
+    },
+  });
+
+  const submit = async (v: SubmitValue) => {
+    context.closeContextModal(id);
+    await axios.post('/request-reset-password', {
+      email: v.email,
+      username: v.username,
+    }).then(res => res.data);
+  }
+
+  return (
+    <>
+    <form onSubmit={form.onSubmit((v) => submit(v))}>
+      <TextInput
+        {...form.getInputProps('username')}
+        data-autofocus
+        placeholder="Username"
+        label="Username"
+        radius="xl"
+        size="md"
+        mb="sm"
+        inputWrapperOrder={['label', 'input', 'description', 'error']}
+      >
+      </TextInput>
+      <TextInput 
+        {...form.getInputProps('email')}
+        placeholder="Email"
+        description="Please enter the email address you registered for your account."
+        label="Email"
+        radius="xl"
+        size="md"
+        inputWrapperOrder={['label', 'input', 'description', 'error']}
+      >
+      </TextInput>
+      <Button
+        fullWidth 
+        type="submit" 
+        my="lg"
+      >
+        Register
+      </Button>
+    </form>
+    <Alert icon={<IconAlertCircle size="1rem"/>} title="" color="lime">
+      If you have not registered your e-mail address, please contact the administrator.
+    </Alert>
+    </>
+  )
+}
---
+++ packages/frontend/src/pages/reset-password.tsx
@@ -0,0 +1,96 @@
+import { useEffect } from "react";
+import { useNavigate, useSearchParams } from "react-router-dom";
+import { Button, Center, PasswordInput, Stack, TextInput, Title } from "@mantine/core";
+import { isNotEmpty, matchesField, useForm } from "@mantine/form";
+import { modals } from '@mantine/modals';
+import axios from "@/plugins/axios";
+
+interface SubmitValue {
+  username: string,
+  newPassword: string,
+}
+
+export default function ResetPassword() {
+  const [ searchParams ] = useSearchParams();
+  const navigate = useNavigate();
+  const token = searchParams.get("token");
+  const form = useForm({
+    initialValues: {
+      username: '',
+      newPassword: '',
+      confirmNewPassword: '',
+    },
+    validate: {
+      newPassword: isNotEmpty('Password is required'),
+      confirmNewPassword: matchesField('newPassword', 'Passwords are not the same'),
+    }
+  });
+  const modal = () => modals.openContextModal({
+    modal: 'requestResetPassword',
+    title: 'Reset password',
+    innerProps: {},
+  });
+
+  const submit = async (v: SubmitValue) => {
+    const message = await axios.post('/reset-password', {
+      token,
+      newPassword: v.newPassword,
+    }).then(res => res.data);
+
+    if (message == 'succeed reset password') {
+      localStorage.setItem('uid', '');
+      localStorage.setItem('accessToken', '');
+      localStorage.setItem('refreshToken', '');
+      navigate('/');
+    }
+  };
+
+  useEffect(() => {
+    if (!token) {
+      navigate('/');
+      modal();
+    }
+  }, []);
+
+  return (
+    <Center>
+      <Stack justify="center" w={500}>
+        <Title>Password Reset Form</Title>
+        <form onSubmit={form.onSubmit((v) => submit(v))}>
+          <TextInput 
+            {...form.getInputProps('username')}
+            placeholder="Username"
+            label="Username"
+            radius="xl"
+            size="md"
+            inputWrapperOrder={['label', 'input', 'description', 'error']}
+          />
+          <PasswordInput
+            {...form.getInputProps('newPassword')}
+            placeholder="New password"
+            label="New password"
+            radius="xl"
+            size="md"
+            mt="sm"
+          />
+          <PasswordInput
+            {...form.getInputProps('confirmNewPassword')}
+            placeholder="Confirm password"
+            label="Confirm password"
+            radius="xl"
+            size="md"
+            mt="sm"
+          />
+          <Button 
+            fullWidth 
+            type="submit" 
+            mt="lg"
+            disabled={!form.isValid()}
+          >
+            Register
+          </Button>
+        </form>
+      </Stack>
+    </Center>
+  );
+}
