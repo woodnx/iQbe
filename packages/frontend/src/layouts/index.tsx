3f77a3d691eab31e066998d14a7c2388fc308632
woodnext <shun20020611@yahoo.co.jp>
2023-11-27 18:04:08 +0900

    パスワードリセット機能を実装

--- packages/frontend/src/layouts/index.tsx
+++ packages/frontend/src/layouts/index.tsx
@@ -1,15 +1,16 @@
-import { Route, Routes, useLocation } from "react-router-dom";
-import Home from "../pages/home";
-import Login from "../pages/login";
-import Search from "../pages/search";
-import Practice from "../pages/practice";
-import Favorite from "../pages/favorite";
-import DefaultLayout from "./default";
-import DraftLayout from "./draft";
-import History from "../pages/history";
-import Mylist from "../pages/mylists";
 import { useEffect } from "react";
-import Error from "../pages/error";
+import { Route, Routes, useLocation } from "react-router-dom";
+import Home from "@/pages/home";
+import Login from "@/pages/login";
+import Search from "@/pages/search";
+import Practice from "@/pages/practice";
+import Favorite from "@/pages/favorite";
+import DefaultLayout from "@/layouts/default";
+import DraftLayout from "@/layouts/draft";
+import History from "@/pages/history";
+import Mylist from "@/pages/mylists";
+import ResetPassword from "@/pages/reset-password"
+import Error from "@/pages/error";
 
 const defineTitle = (pathname: string) => {
   if (pathname === '/') return 'Home';
@@ -19,18 +20,42 @@ const defineTitle = (pathname: string) => {
   else if (pathname === '/history') return 'History';
   else if (pathname.includes('mylist')) return 'Mylist';
   else if (pathname === '/login') return 'Login';
+  else if (pathname === '/reset-password') return 'Reset password';
   else return '';
 }
 
+const requireDraftLayoutPages = [
+  '/login',
+  '/reset-password'
+];
+
 export default function Root() {
   const location = useLocation();
-  const requiredLogin = location.pathname !== '/login';
-
+  const requiredLogin = !requireDraftLayoutPages.includes(location.pathname);
   const Layout = requiredLogin ? DefaultLayout : DraftLayout;
+
+  // const modal = () => modals.openConfirmModal({
+  //   title: 'Reset Password',
+  //   children: 
+  //   <Text>
+  //     パスワード再設定
+  //   </Text>,
+  //   labels: {
+  //     confirm: 'Confirm',
+  //     cancel: 'Cancel'
+  //   }
+  // })
 
   useEffect(() => {
     document.title = `${defineTitle(location.pathname)} | iQbe`;
   }, [location]);
+
+  // useEffect(() => {
+  //   if (location.pathname === '/reset-password' && !searchParams.get('token')) {
+  //     navigate('/');
+  //     modal();
+  //   }
+  // }, [])
 
   return (
     <Routes>
@@ -42,6 +67,7 @@ export default function Root() {
         <Route path="/history" element={<History />}/>
         <Route path="/mylist/:mid" element={<Mylist />}/>
         <Route path="/login" element={<Login />}/>
+        <Route path="/reset-password" element={<ResetPassword />}/>
         <Route path="*" element={<Error />} />
       </Route>
     </Routes>
