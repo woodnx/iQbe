9829daed874b768bdbaebfcd7bfcabd4c1cd72c4
woodnext <shun20020611@yahoo.co.jp>
2024-01-02 17:20:20 +0900

    クイズの編集機能を追加

--- packages/backend/src/routes/quizzes.ts
+++ packages/backend/src/routes/quizzes.ts
@@ -45,17 +45,28 @@ router.get('/:listName?', async (req: QuizRequest, res) => {
     let query = db.selectFrom('quizzes')
     .leftJoin('workbooks', 'quizzes.workbook_id', 'workbooks.id')
     .leftJoin('levels', 'workbooks.level_id', 'levels.id')
+    .leftJoin('users', 'users.id', 'quizzes.creator_id')
+    .leftJoin('quizzes_categories', 'quizzes_categories.quiz_id', 'quizzes.id')
+    .leftJoin('quiz_visible_users', 'quiz_visible_users.quiz_id', 'quizzes.id')
     .select(({ fn }) => [
       'quizzes.id as id',
       'quizzes.que as question',
       'quizzes.ans as answer',
       'workbooks.name as workbook', 
+      'workbooks.wid as wid', 
       'workbooks.date as date',
       'levels.color as level',
+      'users.uid as creator',
+      'quizzes_categories.category_id as category',
+      'quizzes_categories.sub_category_id as subCategory',
       fn.countAll<number>().over().as('size'),
     ]
       //{ total: knex.raw('quizzes.total_crct_ans + quizzes.total_wrng_ans + quizzes.total_through_ans')},
-    );
+    )
+    .where(({ eb, or }) => or([
+      eb('quiz_visible_users.user_id', 'is', null),
+      eb('quiz_visible_users.user_id', '=', userId),
+    ]));
 
     if (!!workbook) query = query.where('workbooks.wid', 'in', workbook);
     if (!!level)    query = query.where('levels.id', 'in', level);
@@ -162,6 +173,15 @@ router.get('/:listName?', async (req: QuizRequest, res) => {
         ]))
         .executeTakeFirst();
 
+        const visibleUser = await db
+        .selectFrom('quiz_visible_users')
+        .innerJoin('users', 'users.id', 'quiz_visible_users.user_id')
+        .select([ 'uid' ])
+        .where(({ eb, and }) => and([
+          eb('quiz_id', '=', quiz.id),
+        ]))
+        .executeTakeFirst();
+
         const isFavorite = favorites.some(f => f.quiz_id == quiz.id);
         const registerdMylist = mylists.filter(m => m.quiz_id == quiz.id).map(m => m.mylist_id);
 
@@ -171,6 +191,7 @@ router.get('/:listName?', async (req: QuizRequest, res) => {
           ...right,
           isFavorite,
           registerdMylist,
+          isPublic: !visibleUser,
         };
     }));
 
@@ -184,9 +205,9 @@ router.get('/:listName?', async (req: QuizRequest, res) => {
 router.post('/', async (req, res) => {
   const question: string | undefined = req.body.question;
   const answer: string | undefined = req.body.answer;
-  const isPrivate: boolean = !(req.body.visible);
-  const category = req.body.category;
-  const subCategory = req.body.subCategory;
+  const isPrivate = !(req.body.visible);
+  const category = Number(req.body.category);
+  const subCategory = category !== 0 ? Number(req.body.subCategory) : 0;
   const workbook = req.body.workbook;
   const limitedUser: number[] = req.body.limitedUser;
   const userId = req.userId;
@@ -196,60 +217,219 @@ router.post('/', async (req, res) => {
     return;
   }
   
-  await db.transaction().execute(async (trx) => {
-    const workbookId = (await trx
-    .selectFrom('workbooks')
-    .select('id')
-    .where('wid', '=', workbook)
-    .executeTakeFirst())?.id;
+  try {
+    const newQuiz = await db.transaction().execute(async (trx) => {
+      const workbookId = (await trx
+      .selectFrom('workbooks')
+      .select('id')
+      .where('wid', '=', workbook)
+      .executeTakeFirst())?.id;
 
-    await trx.insertInto('quizzes')
-    .values({
-      que: question,
-      ans: answer,
-      creator_id: userId,
-      workbook_id: workbookId,
-    })
-    .executeTakeFirst();
-
-    const createdQuiz = await trx.selectFrom('quizzes').select('id')
-    .where(({eb, and}) => and([
-      eb('que', '=', question),
-      eb('ans', '=', answer),
-    ]))
-    .executeTakeFirstOrThrow();
-
-    if (!!category && !!subCategory) {
-      await trx.insertInto('quizzes_categories')
+      await trx.insertInto('quizzes')
       .values({
-        quiz_id: createdQuiz.id,
+        que: question,
+        ans: answer,
+        creator_id: userId,
+        workbook_id: workbookId,
+      })
+      .executeTakeFirst();
+
+      const createdQuiz = await trx.selectFrom('quizzes').select('id')
+      .where(({eb, and}) => and([
+        eb('que', '=', question),
+        eb('ans', '=', answer),
+      ]))
+      .executeTakeFirstOrThrow();
+
+      if (!!category && !!subCategory) {
+        await trx.insertInto('quizzes_categories')
+        .values({
+          quiz_id: createdQuiz.id,
+          category_id: category,
+          sub_category_id: subCategory,
+          user_id: userId,
+        })
+        .executeTakeFirst();
+      }
+
+      if (isPrivate) {
+        const visibleUser = limitedUser || [ userId ];
+
+        visibleUser.forEach(userId => {
+          trx.insertInto('quiz_visible_users')
+          .values({
+            quiz_id: createdQuiz.id,
+            user_id: userId,
+          })
+          .execute();
+        });
+      }
+
+      return await trx.selectFrom('quizzes')
+      .leftJoin('workbooks', 'quizzes.workbook_id', 'workbooks.id')
+      .leftJoin('levels', 'workbooks.level_id', 'levels.id')
+      .leftJoin('users', 'users.id', 'quizzes.creator_id')
+      .leftJoin('quizzes_categories', 'quizzes_categories.quiz_id', 'quizzes.id')
+      .leftJoin('quiz_visible_users', 'quiz_visible_users.quiz_id', 'quizzes.id')
+      .select([
+        'quizzes.id as id',
+        'quizzes.que as question',
+        'quizzes.ans as answer',
+        'workbooks.name as workbook', 
+        'workbooks.wid as wid', 
+        'workbooks.date as date',
+        'levels.color as level',
+        'users.uid as creator',
+        'quizzes_categories.category_id as category',
+        'quizzes_categories.sub_category_id as subCategory',
+      ])
+      .where('quizzes.id', '=', createdQuiz.id)
+      .executeTakeFirstOrThrow();
+    });
+
+    res.status(200).send(newQuiz);
+  } catch(e) {
+    console.error(e);
+  }
+});
+
+router.put('/', async (req, res) => {
+  const quizId = req.body.quizId;
+  const question: string | undefined = req.body.question;
+  const answer: string | undefined = req.body.answer;
+  const isPrivate = !(req.body.visible);
+  const category = Number(req.body.category);
+  const subCategory = category !== 0 ? Number(req.body.subCategory) : 0;
+  const workbook = req.body.workbook;
+  const limitedUser: number[] = req.body.limitedUser;
+  const userId = req.userId;
+
+  if (!question || !answer || !quizId) {
+    res.send('no body parameter');
+    return;
+  }
+
+  try {
+    const updatedQuiz = await db.transaction().execute(async (trx) => {
+      const workbookId = (await trx
+      .selectFrom('workbooks')
+      .select('id')
+      .where('wid', '=', workbook)
+      .executeTakeFirst())?.id;
+
+      await trx.updateTable('quizzes')
+      .set({
+        que: question,
+        ans: answer,
+        creator_id: userId,
+        workbook_id: workbookId,
+      })
+      .where('id', '=', quizId)
+      .executeTakeFirst();
+
+      const oldCategory = await trx
+      .selectFrom('quizzes_categories')
+      .selectAll()
+      .where(({ eb, and }) => and([
+        eb('quiz_id', '=', quizId),
+        eb('user_id', '=', userId)
+      ]))
+      .executeTakeFirst();
+
+      const categoryData = {
+        quiz_id: quizId,
         category_id: category,
         sub_category_id: subCategory,
         user_id: userId,
-      })
-      .executeTakeFirst();
-    }
+      }
 
-    const { id: quizId } = await trx.selectFrom('quizzes')
-    .select('id')
-    .where('que', '=', question)
-    .executeTakeFirstOrThrow();
+      if (category != null && subCategory != null) {
+        if (!!oldCategory) {
+          await trx.updateTable('quizzes_categories')
+          .set(categoryData)
+          .where('quiz_id', '=', quizId)
+          .execute();
+        } else {
+          await trx.insertInto('quizzes_categories')
+          .values(categoryData)
+          .executeTakeFirst();
+        }
+      }
 
-    if (isPrivate) {
-      const visibleUser = limitedUser || [ userId ];
+      const oldVisibleUser = (await trx
+      .selectFrom('quiz_visible_users')
+      .selectAll()
+      .where('quiz_id', '=', quizId)
+      .execute()).map(u => u.user_id);
 
-      visibleUser.forEach(userId => {
-        trx.insertInto('quiz_visible_users')
-        .values({
-          quiz_id: quizId,
-          user_id: userId,
-        });
-      });
-    }
+      const published = oldVisibleUser.length == 0;
 
-    res.send({ message: 'Successfully submitted' })
-    return;
-  });
+      if (isPrivate) {
+        if (published) {
+          const visibleUser = limitedUser || [ userId ];
+
+          visibleUser.forEach(userId => {
+            trx.insertInto('quiz_visible_users')
+            .values({
+              quiz_id: quizId,
+              user_id: userId,
+            })
+            .execute();
+          });
+        } else if(!!limitedUser) {
+          const visibleUser = limitedUser.filter(u => oldVisibleUser.includes(u));
+          const unVisibleUser = oldVisibleUser.filter(u => limitedUser?.includes(u));
+
+          visibleUser.forEach(userId => {
+            trx.insertInto('quiz_visible_users')
+            .values({
+              quiz_id: quizId,
+              user_id: userId,
+            })
+            .execute();
+          });
+
+          unVisibleUser.forEach(userId => {
+            trx.deleteFrom('quiz_visible_users')
+            .where(({ eb, and }) => and([
+              eb('quiz_id', '=', quizId),
+              eb('user_id', '=', userId),
+            ]))
+            .execute();
+          });
+        }
+      } else { // publicに変更
+        await trx.deleteFrom('quiz_visible_users')
+        .where('quiz_id', '=', quizId)
+        .execute();
+      }
+
+      return await trx.selectFrom('quizzes')
+      .leftJoin('workbooks', 'quizzes.workbook_id', 'workbooks.id')
+      .leftJoin('levels', 'workbooks.level_id', 'levels.id')
+      .leftJoin('users', 'users.id', 'quizzes.creator_id')
+      .leftJoin('quizzes_categories', 'quizzes_categories.quiz_id', 'quizzes.id')
+      .leftJoin('quiz_visible_users', 'quiz_visible_users.quiz_id', 'quizzes.id')
+      .select([
+        'quizzes.id as id',
+        'quizzes.que as question',
+        'quizzes.ans as answer',
+        'workbooks.name as workbook', 
+        'workbooks.wid as wid', 
+        'workbooks.date as date',
+        'levels.color as level',
+        'users.uid as creator',
+        'quizzes_categories.category_id as category',
+        'quizzes_categories.sub_category_id as subCategory',
+      ])
+      .where('quizzes.id', '=', quizId)
+      .executeTakeFirstOrThrow();
+    });
+
+    res.status(200).send(updatedQuiz);
+  } catch(e) {
+    console.error(e);
+  }
 });
 
 module.exports = router
--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -1,15 +1,26 @@
 import { Notifications } from "@mantine/notifications";
 import { BrowserRouter as Router } from "react-router-dom";
 import Layout from './layouts';
+import { ModalsProvider } from "@mantine/modals";
+import QuizEditModal from "./components/QuizEditModal";
+
+const modals = {
+  quizEdit: QuizEditModal,
+};
+
+declare module '@mantine/modals' {
+  export interface MantineModalsOverride {
+    modals: typeof modals;
+  }
+}
 
 export default function App() {
-
   return (
-    <>
+    <ModalsProvider modals={modals}>
       <Notifications position="top-right" />
       <Router>
         <Layout />
       </Router> 
-    </>
+    </ModalsProvider>
   );
 }
