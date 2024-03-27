c3b717f386d4cf72260cd54c59fa02067dfaab0a
woodnext <shun20020611@yahoo.co.jp>
2024-03-27 15:05:35 +0900

    feat(api): @api/typesでのオプショナルなプロパティに対してNullableを追加

--- packages/api/tsup.config.ts
+++ packages/api/tsup.config.ts
@@ -6,8 +6,8 @@ export default defineConfig({
   entry: {
     index: 'src/index.ts', 
     predicates: 'src/predicates.ts',
-    types: 'src/generated/@types/index.ts',
-    schema: 'src/generated/schema-format.ts',
+    types: 'src/generated/@types/index.formatted.ts',
+    schema: 'src/generated/schema.formatted.ts',
   },
   clean: true,
   dts: true,
