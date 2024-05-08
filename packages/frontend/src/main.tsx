f2d4bb1375aa72cbe29e4279a6443b43d3d7785e
woodnext <shun20020611@yahoo.co.jp>
2024-05-08 11:36:57 +0900

    fix(frontend): CSSを適用

--- packages/frontend/src/main.tsx
+++ packages/frontend/src/main.tsx
@@ -1,11 +1,13 @@
 import '@mantine/core/styles.css';
+import '@mantine/dates/styles.css';
+import '@mantine/notifications/styles.css';
+import '@mantine/carousel/styles.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { MantineProvider } from '@mantine/core';
 import { IsMobileProvider } from './contexts/isMobile'
 import { RequestResetPasswordProvider } from './contexts/requestResetPassword';
 import App from './App.tsx';
-
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
