b0187dd7538efacd236699f39a3d296d98fd5660
woodnext <shun20020611@yahoo.co.jp>
2024-08-20 05:19:24 +0000

    Add tags route

---
+++ packages/backend/src/routes/tags.ts
@@ -0,0 +1,19 @@
+import TagService from '@/domains/Tag/TagService';
+import TagController from '@/interfaces/controllers/TagController';
+import KyselyClientManager from '@/interfaces/infra/kysely/KyselyClientManager';
+import TagInfra from '@/interfaces/infra/TagInfra';
+import express from 'express';
+
+const router = express.Router();
+
+const tagController = new TagController(
+  new TagService(),
+  new TagInfra(new KyselyClientManager()),
+)
+
+router.get('/', tagController.get());
+router.post('/', tagController.post());
+router.put('/', tagController.put());
+router.delete('/', tagController.delete());
+
+export default router;
