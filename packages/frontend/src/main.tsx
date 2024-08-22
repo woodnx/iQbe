ebfd5f4c1f904f5ac37116cc191728fe1382a4f9
woodnext <shun20020611@yahoo.co.jp>
2024-08-22 10:07:18 +0000

    Add package(@mantine/dropzone)

--- packages/frontend/src/main.tsx
+++ packages/frontend/src/main.tsx
@@ -2,6 +2,7 @@ import '@mantine/core/styles.css';
 import '@mantine/dates/styles.css';
 import '@mantine/notifications/styles.css';
 import '@mantine/carousel/styles.css';
+import '@mantine/dropzone/styles.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { MantineProvider } from '@mantine/core';
