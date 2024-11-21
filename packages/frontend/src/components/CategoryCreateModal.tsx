829d706ee20f312c2688dd6f92ba08aec19816c8
woodnext <shun20020611@yahoo.co.jp>
2024-11-21 14:55:49 +0000

    feat(frontend): 楽観的更新に対応

--- packages/frontend/src/components/CategoryCreateModal.tsx
+++ packages/frontend/src/components/CategoryCreateModal.tsx
@@ -1,6 +1,10 @@
-import { ContextModalProps } from "@mantine/modals";
-import CategoryEditForm from "./CategoryEditForm";
-import { $api } from "@/utils/client";
+import { components } from 'api/schema';
+
+import { $api } from '@/utils/client';
+import { ContextModalProps } from '@mantine/modals';
+import { useQueryClient } from '@tanstack/react-query';
+
+import CategoryEditForm from './CategoryEditForm';
 
 export interface CategoryCreateModalInnerProps<T extends boolean> {
   name: string,
@@ -9,13 +13,32 @@ export interface CategoryCreateModalInnerProps<T extends boolean> {
   parentId: T extends true ? number : undefined,
 }
 
+type Category = components['schemas']['Category'];
+
 export default function CategoryCreateModal<T extends boolean>({
   context,
   id: modalId,
   innerProps,
 }: ContextModalProps<CategoryCreateModalInnerProps<T>>) {
   const { ...formProps } = innerProps;
-  const { mutateAsync: edit } = $api.useMutation("post", "/categories");
+  const queryClient = useQueryClient();
+  const queryKey = ["get", "/categories", undefined];
+
+  const { mutate: edit } = $api.useMutation("post", "/categories", {
+    onMutate: async ({ body }) => {
+      const previous = queryClient.getQueryData(queryKey);
+
+      queryClient.setQueryData(queryKey, (old: Category[]) => [
+        ...old, 
+        body
+      ]);
+
+      return { previous };
+    },
+    onSettled: () => {
+      queryClient.invalidateQueries({ queryKey })
+    },
+  });
 
   const submit = (
     name: string,
--- packages/frontend/src/components/CategoryEditModal.tsx
+++ packages/frontend/src/components/CategoryEditModal.tsx
@@ -1,6 +1,10 @@
-import { ContextModalProps } from "@mantine/modals";
-import CategoryEditForm from "./CategoryEditForm";
-import { $api } from "@/utils/client";
+import { components } from 'api/schema';
+
+import { $api } from '@/utils/client';
+import { ContextModalProps } from '@mantine/modals';
+
+import CategoryEditForm from './CategoryEditForm';
+import { useQueryClient } from '@tanstack/react-query';
 
 export interface CategoryEditModalInnerProps<T extends boolean> {
   name: string,
@@ -10,13 +14,32 @@ export interface CategoryEditModalInnerProps<T extends boolean> {
   id: number,
 }
 
+type Category = components['schemas']['Category'];
+
 export default function CategoryEditModal<T extends boolean>({
   context,
   id: modalId,
   innerProps,
 }: ContextModalProps<CategoryEditModalInnerProps<T>>) {
   const { id, ...formProps } = innerProps;
-  const { mutateAsync: edit } = $api.useMutation("put", "/categories/{id}");
+  const queryClient = useQueryClient();
+  const queryKey = ["get", "/categories", undefined];
+
+  const { mutate: edit } = $api.useMutation("put", "/categories/{id}", {
+    onMutate: async ({ body }) => {
+      const previous = queryClient.getQueryData(queryKey);
+
+      queryClient.setQueryData(queryKey, (old: Category[] | undefined) => {
+        const data = old?.map(c => c.id === id ? { ...c, ...body } : c);
+        return data;
+      });
+
+      return { previous };
+    },
+    onSettled: () => {
+      queryClient.invalidateQueries({ queryKey });
+    },
+  },);
 
   const submit = (
     name: string,
