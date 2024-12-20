e483aa2eb844eba84ad877fecbc75e795711a5a7
woodnext <shun20020611@yahoo.co.jp>
2024-12-20 15:43:53 +0000

    fix: build時に画像が失われる問題を修正 Fixes #188

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -25,7 +25,7 @@ app.use(cors({
 }));
 app.use(express.json()); 
 app.use(boolParser());
-app.use(express.static(path.join(__dirname, 'public'))); 
+app.use(express.static(path.join(process.cwd(), 'public'))); 
 app.use(express.static(path.join(__dirname, 'web')));
 
 app.use(
