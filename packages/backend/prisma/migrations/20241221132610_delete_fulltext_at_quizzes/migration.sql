e311b8b25b0c5aa3290d63c85727eac443e1c862
woodnext <shun20020611@yahoo.co.jp>
2024-12-22 14:24:28 +0000

    feat(backend): question に全文検索を設定する Fixes #186

---
+++ packages/backend/prisma/migrations/20241221132610_delete_fulltext_at_quizzes/migration.sql
@@ -0,0 +1,11 @@
+-- DropIndex
+DROP INDEX `quizzes_ans_idx` ON `quizzes`;
+
+-- DropIndex
+DROP INDEX `quizzes_que_ans_anoans_idx` ON `quizzes`;
+
+-- DropIndex
+DROP INDEX `quizzes_que_ans_idx` ON `quizzes`;
+
+-- DropIndex
+DROP INDEX `quizzes_que_idx` ON `quizzes`;
