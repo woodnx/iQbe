221aee0cb43c7f050aa8ac3666989b97dfcd8306
woodnext <shun20020611@yahoo.co.jp>
2025-01-01 02:34:56 +0000

    fix(frontend): Routerのアクセス制限をつける Fixes #196

---
+++ packages/frontend/src/hooks/useAuth.ts
@@ -0,0 +1,18 @@
+import { components } from 'api/schema';
+import store from 'storejs';
+
+type User = components["schemas"]["User"];
+
+export const useAuth = () => {
+  const setUser = (user: User, accessToken: string, refreshToken: string) => {
+    store.set('loginedUser', {
+      ...user,
+      accessToken,
+      refreshToken,
+    });
+  };
+
+  const user: User = store.get('loginedUser');
+
+  return { user, setUser };
+}
