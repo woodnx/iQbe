61e4f3932a4e0a8d825326f981b75c4ace7ae6d6
朴 (Kotsugi) <shun20020611@yahoo.co.jp>
2025-04-06 00:09:28 +0900

    fix: Throws CORS error when production. (#219)
    Fixes #218

--- packages/frontend/src/utils/client.ts
+++ packages/frontend/src/utils/client.ts
@@ -3,7 +3,7 @@ import { paths } from "api/schema";
 import createFetchClient from "openapi-fetch";
 import createClient from "openapi-react-query";
 
-const client = createFetchClient<paths>({ baseUrl: "http://localhost:9000/api/" });
+const client = createFetchClient<paths>({ baseUrl: "/api/" });
 const $api = createClient(client)
 
 client.use(authMiddleware);
