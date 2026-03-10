f8ea8906bfee975fb15ab10bad374286b5fbfeab
woodnext <shun20020611@yahoo.co.jp>
2026-03-10 09:39:11 +0000

    change font: BIZ UDPGothic

--- packages/frontend/src/main.tsx
+++ packages/frontend/src/main.tsx
@@ -3,13 +3,13 @@ import "@mantine/dates/styles.css";
 import "@mantine/notifications/styles.css";
 import "@mantine/carousel/styles.css";
 import "@mantine/dropzone/styles.css";
+import { MantineProvider } from "@mantine/core";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { MantineProvider } from "@mantine/core";
+import App from "./App.tsx";
 import { IsMobileProvider } from "./contexts/isMobile";
 import { RequestResetPasswordProvider } from "./contexts/requestResetPassword";
-import App from "./App.tsx";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 const queryClient = new QueryClient();
 
@@ -18,7 +18,7 @@ ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
     <MantineProvider
       theme={{
         fontFamily:
-          "Hiragino Maru Gothic Pro, Kosugi Maru, BIZ UDGothic, Roboto, HelveticaNeue, Arial, sans-serif",
+          "Hiragino Kaku Gothic ProN, BIZ UDPGothic, Roboto, HelveticaNeue, Arial, sans-serif",
       }}
     >
       <QueryClientProvider client={queryClient}>
