8e674d4d6479826dc595d2da7d284b393df0f1fb
woodnext <shun20020611@yahoo.co.jp>
2026-03-10 11:33:39 +0000

    collection を作成

--- packages/frontend/src/routes/collection.tsx
+++ packages/frontend/src/routes/collection.tsx
@@ -1,6 +1,7 @@
-import { Card, Stack, Text, Title } from "@mantine/core";
-import { createFileRoute } from "@tanstack/react-router";
-import WorkbookCard from "@/features/workbook/components/WorkbookCard";
+import { Card, Group, Stack, Text, Title } from "@mantine/core";
+import { IconBook2, IconList, IconStar } from "@tabler/icons-react";
+import { createFileRoute, useNavigate } from "@tanstack/react-router";
+import dayjs from "dayjs";
 import { $api } from "@/utils/client";
 
 export const Route = createFileRoute("/collection")({
@@ -11,23 +12,108 @@ function RouteComponent() {
   const { data: workbooks } = $api.useQuery("get", "/workbooks");
   const { data: mylists } = $api.useQuery("get", "/mylists");
 
+  const navigate = useNavigate();
+
   return (
     <>
-      <Title size="h2">問題集</Title>
-      <Stack>
-        {workbooks?.map((workbook) => (
-          <WorkbookCard key={workbook.wid} workbook={workbook} />
-        ))}
-      </Stack>
+      <Group mb="xs" ml="sm">
+        <IconBook2 />
+        <Title size="h3">問題集</Title>
+      </Group>
+      <Card radius="lg">
+        <Stack>
+          {workbooks?.map((workbook) => (
+            <Group
+              key={workbook.wid}
+              onClick={(e) => {
+                e.preventDefault();
+                navigate({
+                  to: "/workbook/$wid",
+                  params: { wid: `${workbook.wid}` },
+                });
+              }}
+              style={{
+                cursor: "pointer",
+              }}
+              component="a"
+            >
+              <div
+                style={{
+                  width: "4px",
+                  backgroundColor: "#a5d8ff",
+                  alignSelf: "stretch",
+                  borderRadius: "12px",
+                }}
+              ></div>
+              <div>
+                <Text fw={700} size="lg">
+                  {workbook.name}
+                </Text>
+                <Text c="dimmed" size="sm">
+                  {dayjs(workbook.date).format("YYYY/MM/DD")}
+                </Text>
+              </div>
+            </Group>
+          ))}
+        </Stack>
+      </Card>
 
-      <Title size="h2">マイリスト</Title>
-      <Stack>
-        {mylists?.map((mylist) => (
-          <Card key={mylist.mid} withBorder radius="lg">
-            <Title size="h3">{mylist.name}</Title>
-          </Card>
-        ))}
-      </Stack>
+      <Group align="center" mt="lg" mb="xs" ml="sm">
+        <IconList />
+        <Title size="h3">マイリスト</Title>
+      </Group>
+      <Card radius="lg">
+        <Stack>
+          {mylists?.map((mylist) => (
+            <Group
+              key={mylist.mid}
+              onClick={(e) => {
+                e.preventDefault();
+                navigate({
+                  to: "/mylist/$mid",
+                  params: { mid: `${mylist.mid}` },
+                });
+              }}
+              style={{
+                cursor: "pointer",
+              }}
+              component="a"
+            >
+              <div
+                style={{
+                  width: "4px",
+                  backgroundColor: "#a5d8ff",
+                  alignSelf: "stretch",
+                  borderRadius: "12px",
+                }}
+              ></div>
+              <Text fw={700} size="lg">
+                {mylist.name}
+              </Text>
+            </Group>
+          ))}
+        </Stack>
+      </Card>
+
+      <Card
+        radius="lg"
+        mt="lg"
+        onClick={(e) => {
+          e.preventDefault();
+          navigate({
+            to: "/favorite",
+          });
+        }}
+        style={{
+          cursor: "pointer",
+        }}
+        component="a"
+      >
+        <Group>
+          <IconStar />
+          <Title size="h3">お気に入り</Title>
+        </Group>
+      </Card>
     </>
   );
 }
